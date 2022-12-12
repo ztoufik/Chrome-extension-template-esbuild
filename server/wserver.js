acfd496b5adb9c0bacf1a6db39c13e875fd3b976
ztoufik <toufikouedtlelat@gmail.com>
2022-12-12 15:08:10 +0100

    send string data from client to extension

--- server/wserver.js
+++ server/wserver.js
@@ -1,10 +1,36 @@
 import { WebSocketServer } from 'ws';
 import express from 'express';
+import { Buffer } from 'node:buffer';
 
-const app = express()
-const server_port = 3000
 const ws_port = 8080
 const wss = new WebSocketServer({ port: ws_port });
+
+const launchExpServer=(ws)=>{
+    const app = express()
+    const server_port = 3000
+
+   // parse application/json
+    app.use(express.json());
+
+    app.get('/', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.json({name:"toufik",first_name:"zoubir"})
+        console.log('request arrived');
+    })
+
+
+    app.post('/add', function(req,res) {
+        console.log(req.body.login);
+        const {login,password}=req.body;
+        ws.send(login);
+        res.status(201).json(req.body.name);
+    });
+
+    app.listen(server_port, () => {
+      console.log(`express server listening on port ${server_port}`)
+    })
+}
 
 console.log(`websocket server listening at port ${ws_port}`);
 wss.on('connection', function connection(ws) {
@@ -14,17 +40,7 @@ wss.on('connection', function connection(ws) {
     });
 
     //ws.send('something');
-    app.get('/', (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.json({name:"toufik",first_name:"zoubir"})
-        console.log('request arrived');
-        ws.send('something');
-    })
-
-    app.listen(server_port, () => {
-      console.log(`express server listening on port ${server_port}`)
-    })
+    launchExpServer(ws);
 });
 
 
