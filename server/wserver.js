1bc86dde31d02fef01611fdc6f6903a64dd6e19d
ztoufik <toufikouedtlelat@gmail.com>
2022-12-12 14:24:26 +0100

    use http request to '/' to trigger sending websocket message to extension

--- server/wserver.js
+++ server/wserver.js
@@ -6,23 +6,25 @@ const server_port = 3000
 const ws_port = 8080
 const wss = new WebSocketServer({ port: ws_port });
 
-console.log(`server started at port ${ws_port}`);
+console.log(`websocket server listening at port ${ws_port}`);
 wss.on('connection', function connection(ws) {
     console.log("connection established");
     ws.on('message', function message(data) {
         console.log('received: %s', data);
     });
 
-    ws.send('something');
+    //ws.send('something');
+    app.get('/', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.json({name:"toufik",first_name:"zoubir"})
+        console.log('request arrived');
+        ws.send('something');
+    })
+
+    app.listen(server_port, () => {
+      console.log(`express server listening on port ${server_port}`)
+    })
 });
 
 
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json({name:"toufik",first_name:"zoubir"})
-})
-
-app.listen(server_port, () => {
-  console.log(`Example app listening on port ${server_port}`)
-})
