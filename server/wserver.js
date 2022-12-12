29f890d748f9d5a25e6ed77468d166c479cd1cbc
ztoufik <toufikouedtlelat@gmail.com>
2022-12-12 12:53:31 +0100

    seperate ws/experss server in one executable

--- server/wserver.js
+++ server/wserver.js
@@ -1,8 +1,12 @@
 import { WebSocketServer } from 'ws';
+import express from 'express';
 
-const wss = new WebSocketServer({ port: 8080 });
+const app = express()
+const server_port = 3000
+const ws_port = 8080
+const wss = new WebSocketServer({ port: ws_port });
 
-console.log("server started at port 8080");
+console.log(`server started at port ${ws_port}`);
 wss.on('connection', function connection(ws) {
     console.log("connection established");
     ws.on('message', function message(data) {
@@ -11,3 +15,14 @@ wss.on('connection', function connection(ws) {
 
     ws.send('something');
 });
+
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({name:"toufik",first_name:"zoubir"})
+})
+
+app.listen(server_port, () => {
+  console.log(`Example app listening on port ${server_port}`)
+})
